8548a183ce3a72eec7d196a920d6b7337b5753dd
Vincent Palfy <xpalfy@stuba.sk>
2023-10-04 21:27:47 +0200

    syscall

--- kernel/proc.h
+++ kernel/proc.h
@@ -84,7 +84,7 @@ enum procstate { UNUSED, USED, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 // Per-process state
 struct proc {
   struct spinlock lock;
-
+  int trace_mask;
   // p->lock must be held when using these:
   enum procstate state;        // Process state
   void *chan;                  // If non-zero, sleeping on chan
--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -7,6 +7,16 @@
 #include "proc.h"
 
 uint64
+sys_trace(void){
+  int mask;
+  if(argint(0,&mask)<0){
+  	return -1;
+  }
+  myproc()->trace_mask=mask;
+  return 0;    
+}
+
+uint64
 sys_exit(void)
 {
   int n;
